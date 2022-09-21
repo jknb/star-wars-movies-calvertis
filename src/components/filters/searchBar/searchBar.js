e07d43de622ca0d8b1c2777a2b0d5feee086ebd9
calve <alvertischris@gmail.com>
2022-09-21 22:58:54 +0300

    change input type to search

--- src/components/filters/searchBar/searchBar.js
+++ src/components/filters/searchBar/searchBar.js
@@ -3,9 +3,8 @@ const SearchBar = ({ searchBarInputChanged }) => {
     return (
         <>
             <span>🔍</span>
-            <input type="text" placeholder="Search..." onChange={e => searchBarInputChanged(e.target.value)} />
+            <input type="search" placeholder="Search..." onChange={e => searchBarInputChanged(e.target.value)} />
         </>
-
     );
 }
 
