906a50bd4ce98cd53462f41cb30c55eaa4fa7ca6
calve <alvertischris@gmail.com>
2022-09-23 02:11:37 +0300

    style sortBy Modal - unfinished

--- src/components/filters/sortBy/sortByModal/sortByModal.js
+++ src/components/filters/sortBy/sortByModal/sortByModal.js
@@ -1,13 +1,26 @@
 import './sortByModal.css';
 import { sortByTypes } from '../../../../constants/sortByConstants';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { useState } from 'react';
 
 const SortByModal = ({ closeButtonModalClicked, setMovieAttribute }) => {
+    const [sortBy, setSortBy] = useState(sortByTypes.year);
+
+    const handleSortBy = (event, newSortBy) => {
+        if (newSortBy !== null) {
+            setSortBy(newSortBy);
+        }
+    };
+
     return (
         <div className="modal">
             <div> Sort By </div>
             <span className="close" onClick={closeButtonModalClicked}>&times;</span>
-            <div onClick={() => setMovieAttribute(sortByTypes.episode)}>Episode</div>
-            <div onClick={() => setMovieAttribute(sortByTypes.year)}>Year</div>
+            <ToggleButtonGroup value={sortBy} exclusive onChange={handleSortBy} orientation="vertical" aria-label="sortby button" variant="text">
+                <ToggleButton size="small" value={sortByTypes.episode} onClick={() => setMovieAttribute(sortByTypes.episode)}>Episode</ToggleButton>
+                <ToggleButton size="small" value={sortByTypes.year} onClick={() => setMovieAttribute(sortByTypes.year)}>Year</ToggleButton>
+            </ToggleButtonGroup>
         </div>
     );
 }
