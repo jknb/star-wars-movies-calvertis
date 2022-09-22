b9442a98f4a73600b491048001340294933ec4ee
calve <alvertischris@gmail.com>
2022-09-23 02:12:33 +0300

    style movies
    Add Divider between each movie

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -1,18 +1,20 @@
 import Movie from './movie/movie.js';
+import Divider from '@mui/material/Divider';
 
-const Movies = ({ movies, movieClicked }) => {
-    return (
-        <>
-            {movies.map(movie =>
+const Movies = ({ movies, movieClicked }) => (
+    <>
+        {movies.map(movie =>
+            <div key={movie.episode_id}>
                 <Movie
                     movieClicked={() => movieClicked(movie)}
-                    key={movie.episode_id}
                     episodeId={movie.episode_id}
                     title={movie.title}
                     releaseDate={movie.release_date}
-                />)}
-        </>
-    )
-}
+                />
+                <Divider />
+            </div>
+        )}
+    </>
+);
 
 export default Movies;
