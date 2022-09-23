6a606bfda66d89227654a24420980f3ae8b33306
calve <alvertischris@gmail.com>
2022-09-23 12:03:54 +0300

    Remove return statement

--- src/components/filters/sortBy/sortByButton/sortByButton.js
+++ src/components/filters/sortBy/sortByButton/sortByButton.js
@@ -1,9 +1,7 @@
 import Button from '@mui/material/Button';
 
-const SortByButton = ({ sortByButtonClicked }) => {
-    return (
-        <Button size="small" variant="outlined" onClick={sortByButtonClicked}>Sort By...</Button>
-    );
-}
+const SortByButton = ({ sortByButtonClicked }) => (
+    <Button size="small" variant="outlined" onClick={sortByButtonClicked}>Sort By...</Button>
+);
 
 export default SortByButton;
--- src/components/movies/movieDescription/movieDescription.js
+++ src/components/movies/movieDescription/movieDescription.js
@@ -1,13 +1,11 @@
 import { convertNumberToLatin } from "../../../utils/numberToLatin";
 
-const MovieDescription = ({ episodeId, title, openingCrawl }) => {
-    return (
-        <>
-            <h2>Episode {convertNumberToLatin(episodeId)} - {title}</h2>
-            <div> {openingCrawl} </div>
-        </>
+const MovieDescription = ({ episodeId, title, openingCrawl }) => (
+    <>
+        <h2>Episode {convertNumberToLatin(episodeId)} - {title}</h2>
+        <div> {openingCrawl} </div>
+    </>
 
-    );
-}
+);
 
 export default MovieDescription;
