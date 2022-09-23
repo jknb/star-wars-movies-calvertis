b0643b375564671ee7f338c5c5b2458e5b6079aa
calve <alvertischris@gmail.com>
2022-09-23 12:01:28 +0300

    added parentheses to function return

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,5 +1,7 @@
 import CircularProgress from '@mui/material/CircularProgress';
 
-const Loading = () => <CircularProgress></CircularProgress>
+const Loading = () => (
+    <CircularProgress></CircularProgress>
+);
 
 export default Loading;
