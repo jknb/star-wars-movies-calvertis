45a58db695d25056820adf28dac4941552a8422d
calve <alvertischris@gmail.com>
2022-09-23 12:02:07 +0300

    Remove return statement in component

--- src/components/movies/movie/movie.js
+++ src/components/movies/movie/movie.js
@@ -1,17 +1,15 @@
 import { convertNumberToLatin } from "../../../utils/numberToLatin";
 import './movie.css';
 
-const Movie = ({ episodeId, title, releaseDate, movieClicked }) => {
-    return (
-        <div
-            className="movie"
-            onClick={movieClicked}
-        >
-            <div>EPISODE {episodeId}</div>
-            <div >Episode {convertNumberToLatin(episodeId)} - {title}</div>
-            <div>{releaseDate}</div>
-        </div>
-    );
-}
+const Movie = ({ episodeId, title, releaseDate, movieClicked }) => (
+    <div
+        className="movie"
+        onClick={movieClicked}
+    >
+        <div>EPISODE {episodeId}</div>
+        <div >Episode {convertNumberToLatin(episodeId)} - {title}</div>
+        <div>{releaseDate}</div>
+    </div>
+);
 
 export default Movie;
